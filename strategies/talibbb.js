9923c3746a8ea584d3de7a8f18983cb64902e1ce
Han Zhang <hanz@vmware.com>
2018-11-20 21:13:33 +0800

    Update BB strategy.

--- strategies/talibbb.js
+++ strategies/talibbb.js
@@ -1,28 +1,14 @@
 log = require('../core/log.js');
-const strat = {};
+const strategy = {};
 /*
  * TA-lib optInMAType values:
- *     +-------+---+
- *     | SMA   | 0 |
- *     +-------+---+
- *     | EMA   | 1 |
- *     +-------+---+
- *     | WMA   | 2 |
- *     +-------+---+
- *     | DEMA  | 3 |
- *     +-------+---+
- *     | TEMA  | 4 |
- *     +-------+---+
- *     | TRIMA | 5 |
- *     +-------+---+
- *     | KAMA  | 6 |
- *     +-------+---+
- *     | MAMA  | 7 |
- *     +-------+---+
- *     | T3    | 8 |
- *     +-------+---+
+ *     +-------+---+-------+---+-------+---+-------+---+-------+---+
+ *     | SMA   | 0 | EMA   | 1 | WMA   | 2 | DEMA  | 3 | TEMA  | 4 |
+ *     +-------+---+-------+---+-------+---+-------+---+-------+---+
+ *     | TRIMA | 5 | KAMA  | 6 | MAMA  | 7 | T3    | 8 |
+ *     +-------+---+-------+---+-------+---+-------+---+
  */
-strat.init = function () {
+strategy.init = function () {
   console.log('initiating');
   this.trend = {
     zone: 'none',  // none, top, high, low, bottom
@@ -40,11 +26,11 @@ strat.init = function () {
   this.addTalibIndicator('bb', 'bbands', talibBBSettings);
 };
 
-strat.log = function(candle) {
-  console.log(candle);
+strategy.log = function(candle) {
+  // console.log(candle);
 };
 
-strat.check = function (candle) {
+strategy.check = function (candle) {
   const BB = this.talibIndicators.bb.result;
   const price = candle.close;
 
@@ -53,15 +39,17 @@ strat.check = function (candle) {
   const upper = BB.outRealUpperBand;
   const middle = BB.outRealMiddleBand;
   const lower = BB.outRealLowerBand;
+  console.log(`METRICS: ${candle.start.format()}, ${candle.close}, ${upper}, ${middle}, ${lower}`);
 
   if (price >= upper) zone = 'top';
   if ((price < upper) && (price >= middle)) zone = 'high';
   if ((price > lower) && (price < middle)) zone = 'low';
   if (price <= lower) zone = 'bottom';
-  log.debug('current zone:  ', zone);
+  console.log('previous zone:  ', this.trend.zone);
+  console.log('current zone:  ', zone);
 
   if (this.trend.zone === zone) {
-    // Ain't no zone change
+    // No zone change
     log.debug('persisted');
     this.trend = {
       zone: zone,  // none, top, high, low, bottom
@@ -73,14 +61,14 @@ strat.check = function (candle) {
   }
   else {
     // There is a zone change
-    log.debug('Leaving zone: ', this.trend.zone);
-    if (this.trend.zone === 'top') {
-      log.debug('>>>>> SIGNALING ADVICE LONG <<<<<<<<<<<<');
+    console.log('Leaving zone: ', this.trend.zone);
+    if (zone === 'top') {
+      console.log('>>>>> SIGNALING ADVICE LONG <<<<<<<<<<<<');
       this.advice('long');
     }
 
-    if (this.trend.zone === 'bottom') {
-      log.debug('>>>>> SIGNALING ADVICE SHORT <<<<<<<<<<<<');
+    if (this.trend.zone !== 'bottom' && zone === 'low') {
+      console.log('>>>>> SIGNALING ADVICE SHORT <<<<<<<<<<<<');
       this.advice('short');
     }
 
@@ -94,4 +82,4 @@ strat.check = function (candle) {
   }
 };
 
-module.exports = strat;
+module.exports = strategy;
