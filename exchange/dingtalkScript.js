a10f2dc932a204d6064f3bb4a3af24729679de8a
Han Zhang <ripley.net@gmail.com>
2019-03-07 18:40:55 +0800

    Add a tool script to send account portfolio messages via dingbot.

--- exchange/dingtalkScript.js
+++ exchange/dingtalkScript.js
@@ -17,7 +17,10 @@ const bot = new DINGBOT({
   accessToken: reportConf.dingbotToken
 });
 
-let finalData = [];
+let finalData = {
+  portfolio: [],
+  orders: []
+};
 
 function sendJsonMessage(extendedMsg) {
   let textContent = {
@@ -45,8 +48,8 @@ function sendMdMessage(extendedMsg) {
 }
 
 function convertJsonToMd(dataObjs) {
-  let md = '';
-  dataObjs.forEach(obj => {
+  let md = '#Portfolio概要: \n';
+  dataObjs.portfolio.forEach(obj => {
     md = md + `## ${obj.account}:\n`;
     if (!!obj.data && obj.data.length > 0) {
       obj.data.forEach(entry => {
@@ -57,13 +60,26 @@ function convertJsonToMd(dataObjs) {
     }
   });
 
+  md = '#Orders概要: \n';
+  dataObjs.orders.forEach(obj => {
+    md = md + `## ${obj.account}:\n`;
+    if (!!obj.data && obj.data.length > 0) {
+      obj.data.forEach(entry => {
+        md = md + `### Symbol: ${entry.symbol}\n\n > 类型: ${entry.type} \n\n > 出价: ${entry.price} \n\n > 挂单量: ${entry.original_amount} \n\n > 已成交: ${entry.executed_amount} \n\n > 待成交: ${entry.remaining_amount} \n\n > 时间: ${(new Date(entry.timestamp * 1000)).toLocaleString()} \n\n > 未实现盈亏: ${entry.pl} \n\n\n\n`;
+      })
+    } else {
+      md = md + '> 当前无活动挂单\n\n';
+    }
+  });
+
   return md;
 }
 
-function createCb(account) {
+function createCbForPortfolio(account) {
   return (err, data) => {
-    finalData.push({account: account, data: data});
-    if (finalData.length === credentials.length) {
+    finalData.portfolio.push({account: account, data: data});
+    if (finalData.portfolio.length === credentials.length
+      && finalData.orders.length === credentials.length) {
       // All data received, go ahead to send to dingtalk.
       //sendJsonMessage(JSON.stringify(finalData, null, 2));
       sendMdMessage(convertJsonToMd(finalData))
@@ -72,15 +88,33 @@ function createCb(account) {
   };
 }
 
+function createCbForOrders(account) {
+  return (err, data) => {
+    finalData.orders.push({account: account, data: data});
+    if (finalData.portfolio.length === credentials.length
+      && finalData.orders.length === credentials.length) {
+      // All data received, go ahead to send to dingtalk.
+      //sendJsonMessage(JSON.stringify(finalData, null, 2));
+      sendMdMessage(convertJsonToMd(finalData))
+      // console.log(JSON.stringify(finalData, null, 2));
+    }
+  };
+}
+
+
 function run() {
-  finalData = [];
+  finalData = {
+    portfolio: [],
+    orders: []
+  };
   credentials.forEach(credential => {
     let bfx = new BFX.RESTv1({
       apiKey: credential.key,
       apiSecret: credential.secret,
       transform: true
     });
-    bfx.active_positions(createCb(credential.name));
+    bfx.active_positions(createCbForPortfolio(credential.name));
+    bfx.active_orders(createCbForOrders(credential.name));
   });
 }
 
